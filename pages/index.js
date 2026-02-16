2c4266b0d994c4a127110263d5eaeb7816cd8793
Pedro Brambilla <pedrobrambillax@gmail.com>
2026-02-16 20:01:46 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Fernanda, eu amo voce. Se voce me ama, dá uma risadinha!</h1>
+    return <h1>Fernanda, eu amo voce. Se voce me ama, dá uma risadinha!❤️</h1>
 }
 
 export default Home;
